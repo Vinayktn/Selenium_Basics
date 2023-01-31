42f05ab4395fd515d351aab4c8bc5a8d6acb5feb
VK075855 <Vinay.KumarTN@Cerner.com>
2023-01-31 23:39:59 +0530

    Assignment submission

--- LearnSelenium/src/test/java/CheckBoxes/Assignment_1.java
+++ LearnSelenium/src/test/java/CheckBoxes/Assignment_1.java
@@ -2,10 +2,12 @@ package CheckBoxes;
 
 import java.util.List;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Assignment_1 
 {
@@ -17,12 +19,14 @@ public class Assignment_1
 
 		//1. Check the first Checkbox and verify if it is successfully checked and Uncheck it again to verify if it is successfully Unchecked
 		driver.findElement(By.id("checkBoxOption1")).click();
-		System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
 		driver.findElement(By.id("checkBoxOption1")).click();
-		System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
 
 
 		//2. How to get the Count of number of check boxes present in the page
-		driver.findElements(By.xpath("//input[@type='checkbox']")).size();
+		System.out.println(driver.findElements(By.xpath("//input[@type='checkbox']")).size());
+ 		
+		Assert.assertEquals(driver.findElements(By.xpath("//input[@type='checkbox']")).size(), 3);
 	}
 }
